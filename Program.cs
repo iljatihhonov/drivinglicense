9bec18c71ca7b056146b13161f47085d98f760c0
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-03-18 11:56:08 +0200

    Driving License

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давайка проверим, достаточно ли Вы взрослый для получения водительских прав. В каком годы Вы родились?");
+            string yearOfBirth = Console.ReadLine();
+            int yearOfBirthInt = Convert.ToInt32(yearOfBirth);
+            int age = 2022 - yearOfBirthInt;
+            if (age < 18)
+            {
+                Console.WriteLine("Рановато Вам ещё за руль садиться. Потерпите немного!");
+            }
+            if (age > 18)
+            {
+                Console.WriteLine("Да Вам уже давно можно за руль! Катайтесь осторожно!");
+            }
+            if (age == 18)
+            {
+                Console.WriteLine("Поздравляю, Вам как раз столько, сколько необходимо лет!");
+            }
+        }
+    }
+}
